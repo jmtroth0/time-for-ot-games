80ba5eb5ff288b3b85665e071316d515c5fc1efa
jordan roth <jmtroth0@gmail.com>
2022-06-22 14:30:35 -0700

    pointer

--- src/game_components/board.tsx
+++ src/game_components/board.tsx
@@ -6,11 +6,11 @@ class Board extends React.Component {
     render () {
         return (
                 <div id="board" style={{
-                    position: "absolute", 
-                    top: 0, 
-                    left: 0, 
-                    right: 0, 
-                    bottom: 0, 
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
                     zIndex: -1,
                     backgroundColor: "pink"}}></div>
         )
--- src/game_components/piece.tsx
+++ src/game_components/piece.tsx
@@ -7,7 +7,7 @@ class Piece extends React.Component {
     render () {
         return (
             <Draggable bounds="#board">
-                <div style={{display: "inline-block", fontSize: "60px", color: this.props.color}}>
+                <div style={{display: "inline-block", fontSize: "60px", color: this.props.color, cursor: "pointer"}}>
                     &#9823;
                 </div>
             </Draggable>
