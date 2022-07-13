8ebe805ce94da1f8649b7a10be0ecfa3b8376857
jordan roth <jmtroth0@gmail.com>
2022-07-12 17:26:09 -0700

    try this

--- src/pages/game.tsx
+++ src/pages/game.tsx
@@ -7,61 +7,50 @@ import Feedback from "../assets/tsx/components/feedback";
 import "../assets/scss/main.scss";
 
 
-const GamePage = ({location}) => {
-    let boardImage;
-    let pieceColors;
-    let roller;
-    let numDice;
-    let numSides;
-    let spinnerColors;
-    let instructionsContent;
+const GamePage = ({location, data}) => {
+    let initBoardImage = "CoreStrength";
+    let initPieceColors = ["red", "green", "blue", "yellow"]
+    let initRoller = "dice";
+    let initNumDice = 1;
+    let initNumSides = 6;
+    let initSpinnerColors = ["red", "green", "purple", "orange", "yellow", "blue"];
+    let initInstructionsContent = "this is how you play";
     if (location.state) {
-        boardImage = location.state.config.boardImage;
-        pieceColors = location.state.config.pieceColors || ["red", "green", "blue", "yellow"];
-        roller = location.state.config.roller;
-        if (roller === "dice") {
-            numDice = location.state.config.numDice;
-            numSides = location.state.config.numSides;
-        } else if (roller === "spinner") {
-            spinnerColors = location.state.config.spinnerColors;
-        }
-        instructionsContent = location.state.config.instructionsContent;
-    } else {
-        boardImage = "CoreStrength";
-        pieceColors = ["red", "green", "blue", "yellow"];
-        roller = "dice";
-        numSides = 6;
-        numDice = 1;
-        spinnerColors = ["red", "green", "purple", "orange", "yellow", "blue"];
-        instructionsContent = "this is how you play";
+        initBoardImage = location.state.config.boardImage || initBoardImage;
+        initPieceColors = location.state.config.pieceColors || initPieceColors;
+        initRoller = location.state.config.roller || initRoller;
+        initNumDice = location.state.config.numDice || initNumDice;
+        initNumSides = location.state.config.numSides || initNumSides;
+        initSpinnerColors = location.state.config.spinnerColors || initSpinnerColors;
+        initInstructionsContent = location.state.config.instructionsContent || initInstructionsContent;
     }
 
-    const [boardImage, setBoardImage] = useState("CoreStrength");
-    const [pieceColors, setPieceColors] = useState(["red", "green", "blue", "yellow"]);
-    const [roller, setRoller] = useState("dice");
-    const [numDice, setNumDice] = useState(1);
-    const [numSides, setNumSides] = useState(6);
-    const [spinnerColors, setSpinnerColors] = useState(["red", "green", "purple", "orange", "yellow", "blue"]);
-    const [instructionsContent, setInstructionsContent] = useState("this is how you play");
+    const [boardImage, setBoardImage] = useState(initBoardImage);
+    const [pieceColors, setPieceColors] = useState(initPieceColors);
+    const [roller, setRoller] = useState(initRoller);
+    const [numDice, setNumDice] = useState(initNumDice);
+    const [numSides, setNumSides] = useState(initNumSides);
+    const [spinnerColors, setSpinnerColors] = useState(initSpinnerColors);
+    const [instructionsContent, setInstructionsContent] = useState(initInstructionsContent);
 
 
-    // useEffect(() => {
-    //     if (window.sessionStorage.getItem('config')) {
-    //         let config = JSON.parse(window.sessionStorage.config)
-    //         setBoardImage(config.boardImage);
-    //         setPieceColors(config.pieceColors || ["red", "green", "blue", "yellow"]);
-    //         setRoller(config.roller);
-    //         if (config.roller === "dice") {
-    //             setNumDice(config.numDice);
-    //             setNumSides(config.numSides);
-    //         } else if (roller === "spinner") {
-    //             setSpinnerColors(config.spinnerColors);
-    //         }
-    //         setInstructionsContent(config.instructionsContent);
-    //     }
-
-    //     sessionStorage.config = JSON.stringify(location.state.config);
-    // }, [])
+    useEffect(() => {
+        if (location.state) {
+            sessionStorage.config = JSON.stringify(location.state.config);
+        } else if (window.sessionStorage.getItem('config')) {
+            let config = JSON.parse(window.sessionStorage.config)
+            setBoardImage(config.boardImage);
+            setPieceColors(config.pieceColors || ["red", "green", "blue", "yellow"]);
+            setRoller(config.roller);
+            if (config.roller === "dice") {
+                setNumDice(config.numDice);
+                setNumSides(config.numSides);
+            } else if (roller === "spinner") {
+                setSpinnerColors(config.spinnerColors);
+            }
+            setInstructionsContent(config.instructionsContent);
+        }
+    }, [])
 
     return (
         <main>
